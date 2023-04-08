61a5b6aa38bf428458d252339e92683b0e2b79fb
n4bi10p <snabilshaikh186@gmail.com>
2023-04-08 16:36:50 +0530

    Initial commit

---
+++ evolution_RMX3031.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RMX3031/device.mk)
+
+# Inherit some common Pixel-Experience stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_RMX3031
+PRODUCT_DEVICE := RMX3031
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme X7 Max 5G
+PRODUCT_MANUFACTURER := realme
+EXTRA_UDFPS_ANIMATIONS := true
+# Build info
+BUILD_FINGERPRINT := "realme/RMX3031/RMX3031L1:12/SP1A.210812.016/R.202206242313:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
